30e578e9960728cd2faa10127a25781fd180ccf9
Stavros Anagnostakis <stavrosanagno@gmail.com>
2024-01-10 11:25:05 +0100

    Cart updates

--- script.js
+++ script.js
@@ -1,70 +1,68 @@
+//Items
 let flowers = [
 {
-name: "Roses",
-image: "https://cdn.pixabay.com/photo/2018/02/09/21/46/rose-3142529_640.jpg",
-price: 20,
-qtty: 1,
-inCart:0
+name: "Pink Roses bouquet",
+image: "https://cdn.pixabay.com/photo/2017/02/06/00/55/bouquet-2041844_640.jpg",
+price: 38,
+qtty: 1
 },
 {
-name: "Tulips",
-image: "https://cdn.pixabay.com/photo/2017/04/23/20/36/tulips-2254970_640.jpg",
-price: 25,
-qtty: 1,
-inCart:0
+name: "Tulips mix",
+image: "https://cdn.pixabay.com/photo/2023/11/25/01/00/ai-generated-8410870_640.png",
+price: 42,
+qtty: 1
 },
 {
-name: "Lilies",
-image: "https://cdn.pixabay.com/photo/2016/07/11/21/23/water-lily-1510707_640.jpg",
-price: 19,
-qtty: 1,
-inCart:0
+name: "Lilies bouquet",
+image: "https://cdn.pixabay.com/photo/2021/03/13/12/27/flowers-6091612_640.jpg",
+price: 40,
+qtty: 1
 },
 {
-name: "Violets",
-image: "https://cdn.pixabay.com/photo/2017/05/22/13/36/water-lily-2334209_640.jpg",
-price: 28,
-qtty: 1,
-inCart:0
+name: "Violets collection",
+image: "https://cdn.pixabay.com/photo/2019/06/18/17/54/asters-4282887_640.jpg",
+price: 44,
+qtty: 1
 },
 {
-name: "Daisies",
-image: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg",
-price: 21,
-qtty: 1,
-inCart:0
+name: "Daisies bouquet",
+image: "https://cdn.pixabay.com/photo/2020/08/08/12/10/flowers-5472859_640.jpg",
+price: 35,
+qtty: 1
 },
 {
-    name: "Daisies",
-    image: "https://cdn.pixabay.com/photo/2015/04/19/08/32/marguerite-729510_1280.jpg",
-    price: 21,
-    qtty: 1,
-    inCart:0
-    }
+name: "Orchids collection",
+image: "https://cdn.pixabay.com/photo/2020/01/12/03/08/orchids-4759196_640.jpg",
+price: 51,
+qtty: 1
+
+}
 ];
 
-//let products = document.querySelectorAll(".flowers");
 
+//Display of items
 flowers.forEach(function(flower){
 document.getElementById("result").innerHTML+=
 `
 <div class="card">
   <img src="${flower.image}">
     <div class="script">
-      <h3>${flower.name}</h3><p>€ ${flower.price}</p>           
+      <h3>${flower.name}</h3><h5>€ ${flower.price}</h5>           
       <div class="links cart-button"><button class="go"><i class="fs-4 bi bi-cart-plus"></i> Add to cart</button>  
-      </div>
-      </div>
-      </div>
-        `;
+    </div>
+  </div>
+</div>
+ `;
 })
 
+//Button to add to cart
 let addTocartBtn = document.querySelectorAll(".cart-button");
 
-let cartCount=0;
+//Declare cart and cartcount on navbar
+let cartCount = 0;
 let cart = [];
 
-
+//Button add to cart for each item
 addTocartBtn.forEach(function(btn,i) {
 btn.addEventListener("click",function() {
     addTocart(i);   
@@ -72,33 +70,35 @@ btn.addEventListener("click",function() {
 })
 
 
-
+//Function to add products to cart
 function addTocart(i) {
-    if (cart.find((item) => item.name == flowers[i].name)){
+if (cart.find((item) => item.name == flowers[i].name)){
     flowers[i].qtty++;
-    }else{
-        cart.push(flowers[i]);
-    }
-    //console.log("Item added");
+}else{
+    cart.push(flowers[i]);
+}
     cartCount++;
     createCartInHTML();
     calcTotal();
     updateCartCount();
     };
 
+//Function to update cartcount on navbar
+function updateCartCount() {
+    const totalQtty = cart.reduce((total, item) => total + item.qtty, 0);
+    const cartCountElement = document.querySelector(".cart-count");
+    if (cartCountElement) {
+        cartCountElement.innerText = totalQtty;
+    }
+    }
 
-    function updateCartCount() {
-      const cartCountElement = document.querySelector(".cart-count");
-      if (cartCountElement) {
-        cartCountElement.innerText = cartCount;
-    }
-    }
-  
+//Function to increase item count on navbar
 function increaseCartCount() {
   cartCount += 1;
   updateCartCount();
 }
 
+//Function to decrease item count on navbar
 function decreaseCartCount() {
   if (cartCount > 0) {
       cartCount -= 1;
@@ -106,19 +106,23 @@ function decreaseCartCount() {
   }
 }
 
+//Function to delete item count on navbar
 function removeCartCount(){
-  cartCount = 0;
-    updateCartCount();
-
+    cartCount = 0;
+       
+  updateCartCount();
+ 
 }
 
+
+//Function to increase item quantity 
 function plusQtty(i){
     cart[i].qtty++;
     document.querySelectorAll(".qtty")[i].innerText = cart[i].qtty;
     
 }
 
-
+//Function to decrease item quantity
 function minusQtty(i) {
     if (cart[i].qtty==1 ) {
         cart.splice(i,1); //when qqty==1, we remove the item
@@ -129,48 +133,40 @@ function minusQtty(i) {
     }
 }
 
+//Function to delete item quantity
 function removeQtty(i) {
    cart[i].qtty=1;
    cart.splice(i,1);
-   
 }
 
-    function calcTotal(){
+//Calculation of cart total amount
+function calcTotal(){
     let total=0;
 
-    cart.forEach(function(flower){
-        total=total+(flower.price*flower.qtty);
+  cart.forEach(function(flower){
+      total+=(flower.price*flower.qtty);
    })
 
-   document.getElementById("price").innerHTML=total + "€";
+   document.getElementById("price").innerHTML="€ "+ total ;
+   
   }
 
-
-
-
-   function createCartInHTML(){
-    document.getElementById("cart").innerHTML="";
-    cart.forEach(function(item){
-        document.getElementById("cart").innerHTML+=
-        `
-        <section id="cart">
-         <article class="product">
+//Function to create cart
+function createCartInHTML(){
+document.getElementById("cart").innerHTML="";
+cart.forEach(function(item){
+document.getElementById("cart").innerHTML+=
+  `
+    <div id="cart">
+      <h2 class="product">
         <header>
           <a class="remove">
-            <img src="${item.image}" alt="">
-
+            <img src="${item.image}" alt="${item.name}">
             <h3>Remove</h3>
           </a>
         </header>
-
         <div class="content">
-
-          <h1>${item.name}</h1>
-
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta, numquam quis perspiciatis ea ad omnis provident laborum dolore in atque.
-
-          <div title="You have selected this product to be shipped in the color yellow." style="top: 0" class="color yellow"></div>
-          <div style="top: 43px" class="type small">XXL</div>
+            <h1>${item.name}</h1>
         </div>
         <footer class="content">
           <span class="minus fs-5 bi bi-dash-circle-fill"></span>
@@ -178,14 +174,14 @@ function removeQtty(i) {
           <span class="plus fs-5 bi bi-plus-circle-fill"></span>
           <h2 class="full-price">€ ${item.price}</h2>
         </footer>
-      </article>
-      </section>
+      </h2>
+    </div>
     
 `;
-})
+});
 
-
-   let plusBtns = document.querySelectorAll(".plus");
+//Add event to buttons
+let plusBtns = document.querySelectorAll(".plus");
 
    plusBtns.forEach((plusBtn,i) => {
    plusBtn.addEventListener("click",function(){
@@ -193,10 +189,11 @@ function removeQtty(i) {
     calcTotal();
     increaseCartCount();
 
-   })
+   });
 
 });
-   let minusBtns = document.querySelectorAll(".minus");
+
+let minusBtns = document.querySelectorAll(".minus");
 
    minusBtns.forEach((minusBtn,i) => {
    minusBtn.addEventListener("click",function(){
@@ -204,8 +201,9 @@ function removeQtty(i) {
      calcTotal();
      decreaseCartCount();
      createCartInHTML();
-    })
-})
+     removeCartCount();
+    });
+});
 
 let removeBtns = document.querySelectorAll(".remove");
 
@@ -216,8 +214,9 @@ let removeBtns = document.querySelectorAll(".remove");
      createCartInHTML();
      removeCartCount();
     
-    })
-})
+    });
+});
 
-   }
+
+};
    
